892872f9232d5e16389d27733a3eb2041835211b
Fernando Marina <fernando.m.marina@gmail.com>
2022-10-06 21:02:59 -0300

    fix

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
 import iconinsta from "./icons/instagram_black_logo_icon_147122.png"
 import iconfacebook from "./icons/social_facebook_fb_35.png"
 import iconwhats from "./icons/whatsapp_black_logo_icon_147050.png"
@@ -9,8 +8,6 @@ import "./footer.css"
 export default function FooterComponent() {
   return (
     <>
-
-    
     <div className='containerFooter' >
       <div className='container1'>
 
@@ -22,13 +19,12 @@ export default function FooterComponent() {
         </div>
 
         <div className='iconsA'>
-            <h1 style={{fontSize:"30px"}}>Encuentranos aqui</h1>
-            
+            <h1 style={{fontSize:"30px"}}>Encuentranos aqui</h1>  
             <a  href="https://www.instagram.com/venminimarket/"><i><img src={iconinsta} style={{width:"50px", marginRight:"5px"}}></img></i></a>
             <a  href="https://www.facebook.com/minimarket.ven"><i><img src={iconfacebook} style={{width:"50px", marginRight:"5px"}}></img></i></a>
             <a  href="http://bit.ly/WhatsVenMini"><i><img src={iconwhats} style={{width:"50px", marginRight:"5px"}}></img></i></a>
             <h1 style={{fontSize:"30px"}}>Pregunte aquí</h1>
-            <a href='https://venminimarket.herokuapp.com/contacto' style={{color:"yellow",fontSize:"30px"}}>www.venminimarket.com</a>
+            <a href='www.venminimarket.com/contacto' style={{color:"yellow",fontSize:"30px"}}>www.venminimarket.com</a>
         </div>
 
       </div>
@@ -60,9 +56,7 @@ export default function FooterComponent() {
                         </div>
                 </div>
             </div>
-
       </div>
-   
     </div>
     </>
     
